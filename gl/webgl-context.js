c63775ef90386e75f1c0d5526cbe69b65f69514e
YertleTurtleGit <github@galzone.de>
2020-06-16 19:25:38 +0200

    fixed context

--- gl/webgl-context.js
+++ gl/webgl-context.js
@@ -3,23 +3,15 @@
 class GlContext {
     static getContext(CANVAS_ID) {
         printDebug('getting webgl context...');
-        const WEBGL_CONTEXT_NAMES = ['webgl', 'experimental-webgl', 'webgl', 'experimental-webgl', 'webkit-3d', 'moz-webgl'];
 
         var canvas = document.getElementById(CANVAS_ID);
         var webglContext;
 
-        var index = 0;
-        while (!webglContext) {
-            if (index > WEBGL_CONTEXT_NAMES.length) {
-                WEBGL_USED_CONTEXT = null;
-                return null;
-            }
-            webglContext = canvas.getContext(WEBGL_CONTEXT_NAMES[index]);
-            this.WEBGL_USED_CONTEXT = WEBGL_CONTEXT_NAMES[index];
-            index++;
+        webglContext = canvas.getContext("webgl");
+        this.WEBGL_USED_CONTEXT = "webgl";
 
-            printDebug('used context: ' + this.WEBGL_USED_CONTEXT);
-        }
+        printDebug('used context: ' + this.WEBGL_USED_CONTEXT);
+
         return webglContext;
     }
 }
